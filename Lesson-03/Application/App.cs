d1282494bffdabe4f16b826d9faa4d7e64a2a28e
Verm-V <verm-v@yandex.ru>
2021-10-24 04:33:23 +0600

    Lesson_03. Добавлено логгирование и DI

---
+++ Lesson-03/Application/App.cs
@@ -0,0 +1,30 @@
+﻿using NLog;
+using System;
+
+namespace Lesson_03.Application
+{
+	public class App
+	{
+		private readonly ILogger _logger;
+
+		public App()
+		{
+			_logger = LogManager.GetCurrentClassLogger();
+			_logger.Debug("App service constructed");
+		}
+
+		public void Run()
+		{
+			_logger.Info("App start");
+
+			//Do something
+
+			_logger.Info("App ends");
+
+			Console.WriteLine("Press any key to exit");
+			Console.ReadKey();
+
+		}
+
+	}
+}
--- Lesson-03/Program.cs
+++ Lesson-03/Program.cs
@@ -1,4 +1,8 @@
-﻿using System;
+﻿using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using NLog;
+using NLog.Extensions.Logging;
+using Lesson_03.Application;
 
 namespace Lesson_03
 {
@@ -6,7 +10,36 @@ namespace Lesson_03
 	{
 		static void Main(string[] args)
 		{
-			Console.WriteLine("...");
+			{
+				// Создаем сервисы и конфигурируем их
+				var services = new ServiceCollection();
+				ConfigureServices(services);
+
+				// Запуск приложения
+				using (ServiceProvider serviceProvider = services.BuildServiceProvider())
+				{
+					App app = serviceProvider.GetService<App>();
+					app.Run();
+				}
+
+			}
 		}
+
+		/// <summary>
+		/// Конфигурирует сервисы
+		/// </summary>
+		/// <param name="services">ServiceCollection</param>
+		private static void ConfigureServices(ServiceCollection services)
+		{
+			services.AddTransient<App>();
+
+			// Клиенты для запросов к Агентам метрик
+
+			services.AddLogging(builder =>
+			{
+				builder.AddNLog("nlog.config");
+			});
+		}
+
 	}
 }
